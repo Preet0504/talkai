4edbc645de3afec8d35aef248087ab980634c8dc
Preet Patel <kingpreetpatel@gmail.com>
2026-02-14 00:07:06 -0600

    Agents video call

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -1,21 +1,26 @@
-import { AgentView } from "@/modules/agents/ui/views/agent-view";
+import { AgentsView } from "@/modules/agents/ui/views/agent-view";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { AgentLoadingState } from "@/modules/agents/ui/views/agent-view";
+import { AgentsViewLoading } from "@/modules/agents/ui/views/agent-view";
 import { Suspense } from "react";
 import { AgentsListHeader } from "@/modules/agents/ui/components/agents-list-header";
+import { DEFAULT_PAGE_SIZE, DEFAULT_PAGE } from "@/constants";
 
 const Page = async () => {
 
   const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+  void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions({
+    page: DEFAULT_PAGE,
+    pageSize: DEFAULT_PAGE_SIZE,
+    search: null,
+  }));
   
   return (
     <>
     <AgentsListHeader />
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<AgentLoadingState />}>
-        <AgentView />
+      <Suspense fallback={<AgentsViewLoading />}>
+        <AgentsView />
       </Suspense>
     </HydrationBoundary>
     </>
